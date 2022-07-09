9fab1b83e6a96dd713e58f9189c11ba4ee4b4bb6
LihueelM <lihueel.m@hotmail.com>
2022-07-09 16:39:25 -0300

    Funciones de alto orden

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-/* //-------------Login-------------
+//-------------Login-------------
 
 function login (edad) {
 
@@ -12,38 +12,15 @@ function login (edad) {
 
 if(login(parseInt(prompt("Ingrese su edad: ")))){
     alert("Bienvenido");
-    compra(parseInt(prompt("Ingrese 1 si quiere fernet $720. Ingrese 2 si quiere gancia $640.")), parseInt(prompt("Ingrese que cantidad desea comprar:")))
-
 }
 else{
     alert("No puede ingresar");
     
 }
-//-------------Compra-------------
 
-function compra (bebida, cantidad){
+// Complementario n°2 implementacion de arrays
 
-    if(bebida == 1){
-
-        let resultado = "Usted comprara " + cantidad + " Unidades Fernet"
-        alert(resultado);
-        alert("El valor total de la compra es de: $" + cantidad * 720 );
-    }
-    else if(bebida == 2){
-
-        let resultado = "Usted comprara " + cantidad + " Unidades Gancia"
-        alert(resultado);
-        alert("El valor total de la compra es de: $" + cantidad * 640 );
-    }
-    else{
-        alert("Por favor seleccione una de las 2 bebidas")
-        
-    }
-} */
-
-/* Complementario n°2 implementacion de arrays*/
-
-/* Creo la info del stock para poder tener referencia de que bebida tenemos a la venta, precio, id y stock disponible */
+//Creo la info del stock para poder tener referencia de que bebida tenemos a la venta, precio, id y stock disponible
 
 class Bebida{
     constructor(id,nombre,precio,stock){
@@ -56,20 +33,71 @@ class Bebida{
 
 let listaBebidas = [];
 
-listaBebidas.push(new Bebida (01 , "Fernet", "$1150" , 25));
-listaBebidas.push(new Bebida (02 , "Gancia", "$940" , 20));
-listaBebidas.push(new Bebida (03 , "Vodka", "$1300" , 28));
-listaBebidas.push(new Bebida (04 , "Gin Tonic", "$1850" , 16));
+listaBebidas.push(new Bebida (01 , "Fernet", 1150 , 25));
+listaBebidas.push(new Bebida (02 , "Gancia", 940 , 20));
+listaBebidas.push(new Bebida (03 , "Vodka", 1300 , 28));
+listaBebidas.push(new Bebida (04 , "Gin Tonic", 1850 , 16));
 
 for(let bebidas of listaBebidas){
     console.log(bebidas);
 }
 
+// Entrega de trabajo final n°1 
+
+//En esta parte creare una funcionalidad donde el comprador podra elegir que bebida comprar y mediante un console.log se mostrara la bebida comprada.
+//Tambien se mostrara el stock inicial antes de la compra y el stock luego de esa misma compra para tener un seguimiento del stock.
+
+//-------------Compra-------------
+
+//Seleccion de que bebida quiere comprar
+
+let seleccion = parseInt(prompt("Elija que opcion quiere comprar: \n"+"Fernet : 1\nGancia: 2\nVodka: 3\nGin Tonic: 4 "));
+
+    //Creo la funcion para buscar la bebida solicitada
+    let buscarBebida = (bebida) => { return bebida.id == seleccion }
+    
+    //Traigo la seleccion
+    let bebidaEncontrada = listaBebidas.find(buscarBebida);
+
+    //Si hay stock permite que siga la compra
+    if(bebidaEncontrada.stock <= 0){
+
+        alert("No hay stock")
+        
+    }
+    else{
+
+        //Se informa que se esta comprando, el valor y se pregunta cuantas unidades quiere ordenar
+        let compra = parseInt(prompt(( "La bebida que a elegido es: " +  bebidaEncontrada.nombre  + "\nEl valor por unidad es de: " + bebidaEncontrada.precio + "\nCuantas bebidas quiere ordenar?")));
+        let verificacion = prompt("Usted va a ordernar " + compra + bebidaEncontrada.nombre + " El total es de: " + (bebidaEncontrada.precio * compra) +  " Es correcto? SI | NO").toUpperCase();
+        //Se actualiza el stock
+        if(verificacion == "SI"){
+
+            let nuevoStock = bebidaEncontrada.stock - compra;
+            bebidaEncontrada.stock = nuevoStock;          
+            //Descuento la cantidad de unidades que se compraron.
+            console.log("Nuevo stock es: " + bebidaEncontrada.stock);
+            //Verifico que el objeto se haya actualizado.
+            console.log(bebidaEncontrada);
+        }
+        else{
+            alert("Se cancela la compra")
+        }      
+
+    }
+    
+
+    
+
+    
+    
+
+    
 
 
 
 
-
+ 
 
 
 
